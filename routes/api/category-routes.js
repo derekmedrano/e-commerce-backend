10037836939cbeaa9eac6c59b95f06d3ca0ea7ea
Derek Medrano <derekjmedrano@gmail.com>
2024-02-22 15:32:15 -0600

    added routes, server connection, and seeds

---
+++ routes/api/category-routes.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData)
+});
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(categoryData => {
+    res.json(categoryData)
+  });
+});
+
+router.post('/', (req, res) => {
+  Category.create(req.body)
+    .then((newCategory) => {
+      res.json(newCategory);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  // Calls the update method on the Book model
+  Category.update(
+    req.body,
+    {
+      // Gets the books based on the id given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  // Looks for the books based on id given in the request parameters and deletes the instance from the database
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+module.exports = router;
